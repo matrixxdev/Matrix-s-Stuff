584f0cc2bd47bebef1329f682f496717034247bc
Matrix <166641279+matrixxdev@users.noreply.github.com>
2024-08-23 23:38:03 +0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// Variables
+
+let card1 = document.getElementById('card1');
+let card2 = document.getElementById('card2');
+let card3 = document.getElementById('card3');
+
+function openPage(page) {
+    window.location.href = page + ".html";
+}
